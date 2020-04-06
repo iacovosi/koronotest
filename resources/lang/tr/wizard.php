8a17b8c847a1631805bb20598cbfa6ee642f9545
achara28 <achara28@ucy.ac.cy>
2020-04-06 18:51:31 +0300

    tr translation added

--- resources/lang/tr/wizard.php
+++ resources/lang/tr/wizard.php
@@ -15,174 +15,128 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-    'coronaVirusTest' => 'Koronavirüs muayenesi',
-    'personal' => 'Personel',
-    'medical' => 'Tıp',
-    'comments' => 'Yorum',
-    'personalInfo' => ' Kişisel bilgiler',
+    'coronaVirusTest' => 'COVID-19 Belirtiler Kendini Kontrol',
+    'personal' => 'kişisel',
+    'medical' => 'Medikal',
+    'comments' => 'Yorumlar',
+    'personalInfo' => ' Kişisel Bilgi',
     'email' => 'E-posta',
-    'full' => 'Tam ad',
-    'zip' => 'Posta kodu',
-    'age' => 'Yaş*',
-    'mobileNo' => 'Telefon Numaranız',
-    'gender' => 'Seks*',
-    'male' => 'Adam',
+    'full' => 'Ad Soyad',
+    'zip' => 'posta kodu',
+    'age' => 'yaş (*)',
+    'mobileNo' => 'cep telefonu numarası',
+    'gender' => 'cinsiyet (*)',
+    'male' => 'Erkek',
     'female' => 'Kadın',
-    'other' => 'Diğer (örneğin ishal)',
-    'selection' => 'Lütfen seçiniz',
-    'contact' => 'Son 14 gün içinde enfekte olmuş bir kişiyle iletişim',
-    'travel' => 'Son zamanlarda yurt dışına seyahat ettin mi?',
-    'next' => 'Sonraki',
-    'medicalInfo' => ' Tıbbi bilgi',
-    'bodyTemp' => 'Vücut sıcaklığı Celsius derece*',
-    'symptomsDate' => 'Belirti başlangıcı',
-    'medicalInfoSelection' => 'Lütfen seçiniz',
-    'contact2' => 'Contact with a COVID-19 infected person within 14 days',
-    'chronic' => 'Kronik problemler (diyabet / kalp hastalığı / akciğer hastalığı / kanser / sigara içme / diğer)',
-    'symptomsSelection' => 'Lütfen belirtilerinizi seçin',
-    'malaise' => 'Ιşık',
-    'myalgia' => 'Κas ağrısı',
+    'other' => 'diğer (e.g. ishal)',
+    'gender_other' => 'Söylememeyi tercih etmek',
+    'selection' => 'sizin durumunuz için doğru olanı seçin',
+    'contact' => 'bugünden itibaren 14 gün içinde covid19 enfekte bir kişiyle iletişim',
+    'travel' => 'yenile yurt dışına çıktınız mı?',
+    'next' => 'sonraki ',
+    'medicalInfo' => ' Medikal bilgi',
+    'bodyTemp' => 'vucüt sıcaklığı ölçümü derece (*)',
+    'symptomsDate' => 'bende bu zamanda belirtiler vardı',
+    'medicalInfoSelection' => 'lütfen medikal bilgilerinizi seçiniz',
+    'contact2' => 'bugünden itibaren 14 gün içinde covid19 enfekte bir kişiyle iletişim',
+    'chronic' => ': kronik sorunlar (diyabet, kalp sorunu, akciğer sorunu, kanser, sigara içmek, diğer sorunlar)',
+    'symptomsSelection' => 'lütfen belirtilerinizi seçiniz',
+    'malaise' => 'keyifsizlik',
+    'myalgia' => 'kas ağrısı',
     'cough' => 'öksürük',
-    'breath' => 'Solunum güçlüğü',
-    'chest' => 'Göğüs ağrısı',
+    'breath' => 'nefes alma zorluğu',
+    'chest' => 'göğüs ağrıları',
     'none' => 'Yukarıdakilerin hiçbiri',
-    'previous' => 'Örnek',
-    'test' => 'Sınav',
-
-    'results' => 'Test Sonuçları',
-    'guides' => 'Kıbrıs Cumhuriyeti talimatlarını takip edin',
-    'caseResultsNot' => 'Sonuçlar, Coronavirus (COVID-19) ile enfekte olmadığınızı gösterdi',
-    'personalDrGuides' => 'Kişisel doktorunuzdan talimatlar almalısınız',
-    'caseResultsInfected' => 'Sonuçlar, Coronavirus (COVID-19) ile enfekte olabileceğinizi göstermiştir.',
-    'restriction' => '14 gün boyunca evde kısıtlanmış',
-    'telephoneContact' => '24-48 saat içinde kişisel doktorla telefonla iletişim',
-    'selfMonitoring' => 'Kendinden izleme',
-    'tips' => 'İpuçları',
-    '1420' => 'Talimatlar için 1420 ile iletişime geçin',
-    'probablyInfected' => 'Corona virüsü (COVID-19) bulaşmış olabilir',
-    'healthcare' => 'Gidin ve tıbbi yardım alın!',
-    'systemError' => 'Sistem hatası!',
-    'tryAgain' => 'Sistem hatası! Lütfen daha sonra tekrar deneyin!',
+    'previous' => 'önceki',
+    'test' => 'değerlendirme',
+    'results' => 'test sonuçları ',
+    'systemError' => 'Error Occured in the System!!! Please try again Later!',
+    'tryAgain' => 'birşeyler ters gitti lütfen daha sonra tekrar deneyiniz ',
     'error' => 'hata!',
-    'disclaimer' => 'CDC yönergelerine ve önerilerine dayanmakla birlikte, bu içerik
-                    profesyonel tıbbi tavsiye, teşhis veya tedavinin yerini alması amaçlanmamıştır.
-                    Herhangi bir sorunuz olduğunda daima doktorunuzun veya diğer nitelikli sağlık uzmanınızın tavsiyelerine başvurun.
-                    tıbbi bir durumunuz olabilir.',
+    'disclaimerp1' => 'Yönergelere ve tavsiyelere dayanırken ',
+    'disclaimerp1.5' => 've',
+    'disclaimerp2' => ' bu içeriğin profesyonel tıbbi tavsiye, teşhis veya tedavinin yerini alması amaçlanmamıştır. Tıbbi bir durumla ilgili aklınıza gelebilecek her türlü sorunuz için daima doktorunuzun veya diğer nitelikli sağlık uzmanınızın tavsiyelerine başvurun.',
 
-    'mandatory' => '* İle işaretli olan alanların doldurulması zorunludur, bilgilerin geri kalanı istatistiksel amaçlar için kullanılır',
-    'age_required' => 'Dikkat! Yaş zorunlu bir alandır!',
-    'body_temperature_required' => 'Dikkat! Vücut Sıcaklığı zorunlu bir alandır!',
-    'loss_of_taste' => 'Lezzet Kaybı',
-    'loss_of_smell' => 'Koku Kaybı',
-    'yes' => 'EVET',
-    'no' => 'HAYIR',
-    'flight_country' => 'Lütfen uçtuğunuz ülkeyi seçin:',
-    'demographic' => 'Demografik',
-    'chronic_conditions' => 'Tıbbi',
-    'travelling' => 'Seyahat',
-    'symptoms' => 'Belirtiler',
-    'exposure' => 'Poz',
-    'country' => 'Menşei ülke:',
-    'flight_country' => 'Lütfen uçtuğunuz ülkeyi seçin:',
-    'covid_19_contact' => 'COVID-19 virüsü teşhisi konmuş biriyle, daha önce veya sonra, belirtilerinizden önceki son iki hafta veya 14 gün boyunca yakın temasta bulundunuz mu?',
-    'fever' => 'Ateş',
-    'language_options' => 'Bu test Kıbrıs\'taki kullanıcılar içindir. Diğerleri için üstteki Dil sekmesini kullanın
-                        dil seçenekleri.',
-    'blank_error' => 'boş olamaz, bir sayıdır',
-    'chronic_conditions_questions' => 'Aşağıdaki kronik durumlardan herhangi birine sahip misiniz?
-    (*)?',
-    'cardiovascular' => 'Kardiyovasküler hastalık (hipertansiyon dahil)',
+    'mandatory' => '* ile işaretlenmiş yerler doldurulmalıdır, geriye kalanlar istatistik amaçları ile kullanılacaklar.',
+    'age_required' => 'dikkat! Yaş doldurulması gerekli alandır ',
+//    'body_temperature_required' => 'Attention! The Body Temperature is a required field!',
+    'loss_of_taste' => 'tat alma kaybı',
+    'loss_of_smell' => 'koku alma kaybi ',
+    'demographic' => 'demografik',
+    'chronic_conditions' => 'medikal',
+    'travelling' => 'seyehat',
+    'symptoms' => 'semptomlar',
+    'exposure' => 'Maruz kalma',
+    'country' => 'ülke',
+    'flight_country' => 'gideceğiniz ülkeyi seçiniz',
+    'covid_19_contact' => 'covid19 virüsü teşhisi konulmuş biri veya daha önce birisiyle semptomlarınızdan önceki son iki hafta veya 14 gün boyunca yakın temasta bulundunuzmu?',
+    'fever' => 'ateş',
+
+
+    'language_options' => 'bu test Kıbrıstaki kullanıcılar içinöngörülmüştür. Dili değişmek için yukardaki tuşa basınız',
+    'blank_error' => 'can\'t be blank, is a number',
+    'chronic_conditions_questions' => 'aşağıdaki kronik durumlar size varmı? (*)?',
+    'cardiovascular' => 'kardiyovasküler hastalık (hipertansiyon dahil)',
     'diabetes' => 'diyabet',
-    'lung' => 'Akciğerin kronik hastalıkları',
+    'lung' => 'akciğerin kronik hastalıkları',
     'respiratory' => 'solunum problemleri',
     'cancer' => 'kanser',
-    'immunodeficiency' => 'immün yetmezlik',
-    'close_contact' => 'Yakın temasın tanımı için buraya bakın',
-    'same_household' => 'COVID-19 vakası ile aynı evde yaşayan bir kişi',
-    'direct_contact' => 'Bir COVID-19 vakası ile doğrudan fiziksel teması olan bir kişi (örneğin tokalaşırken)',
-    'unprotected_direct_contact' => 'Bir COVID-19 vakasının bulaşıcı sekresyonları ile korunmasız doğrudan teması olan bir kişi (örn. Öksürmek)
-            kullanılmış kağıt dokulara çıplak elle dokunarak)',
-    'face-to-face' => 'COVID-19 ile 2 metre ve 15 dakika içinde yüz yüze temas etmiş bir kişi',
-    'closed_environment' => 'Kapalı bir ortamda (örneğin, sınıf, toplantı odası, hastane bekleme odası, vb.)
-            COVID-19 kılıfı 15 dakika veya daha uzun süre ve 2 metreden kısa mesafede',
-    'healthcare_worker' => 'Bir COVID-19 vakasına veya laboratuvar çalışanlarına doğrudan bakım sağlayan bir sağlık çalışanı (HCW) veya başka bir kişi
-            tavsiye edilen kişisel koruyucu ekipman (PPE) olmadan veya bir COVID-19 kutusundan numune taşıma
-            KKD\'nin olası ihlali',
+    'immunodeficiency' => 'imün yetmezliği',
+    'close_contact' => 'burada yakin temas’ın tanımını gör',
+    'same_household' => 'covid-19 ile enfekte olan kişi ile ayni evde yaşayan kişi',
+    'direct_contact' => 'covid-19 ile enfekte olan kişi ile direk fiziksel teması olan kişi (el sıkışmak)',
+    'unprotected_direct_contact' => 'covid-19 ile enfekte olan kişi ile direk temas eden kişi (kişinin öksürmesi, kullanılmış kağıt peçetelere dokunmak )',
+    'face-to-face' => 'covid-19 ile enfekte olan kişi işe 2 metre içinde yüzyüze temas',
+    'closed_environment' => 'covid-19 ile enfekte olan kisi ile kapalı bir alanda 15 dakikadan fazla ve 2 metreden az bir alanda olmak(sınıf, toplantı odası, hastahane bekleme odası',
+    'healthcare_worker' => 'covid-19 vakasına doğrudan bakım sağlayan bir sağlık görevlisi veya başka bir kişi veya covid-19 vakasının önerdiği kişisel koruyucu ekipmanlardan veya olası bir ppe ihlali ile numune işleyen laboratuvar çalışanları',
+    'contact_aircraft' => 'COVID-19 ile enfekte olan kisi iki koltuk arasinda (herhangi bir yönde) oturup temas kuran, seyahat arkadaşları veya bakım sağlayan kişiler ve uçakta endeks vakasının bulunduğu bölümde görev yapan mürettebat üyeleri oturmuş (semptomların şiddeti veya davanın hareketi daha kapsamlı maruziyeti gösteriyorsa, oturan yolcu tüm bölümde veya uçaktaki tüm yolcular yakın temas olarak kabul edilebilir) ',
 
-    'contact_aircraft' => 'COVID-19 kasasının iki koltuğunda (herhangi bir yönde) oturan bir uçakta temas, seyahat arkadaşları
-            veya bakım sağlayan kişiler ve uçakta endeks vakasının bulunduğu bölümde görev yapan mürettebat üyeleri
-            oturmuş (semptomların şiddeti veya davanın hareketi daha kapsamlı maruziyeti gösteriyorsa, oturan yolcu
-            tüm bölümde veya uçaktaki tüm yolcular yakın temas olarak kabul edilebilir)',
 
-    'case1' => 'COVID-19\'dan kaynaklanan ciddi hastalık riski düşüktür ve
-                                        ilgili semptomlar',
-    'result1' => 'Koronavirüsünüz olması muhtemel değildir.
-Hala riskinizi en aza indirmek için genel önlemlerin alınmasını öneririz.
-https://www.pio.gov.cy/coronavirus/ adresinde ayrıntılı olarak verilen hastalığa yakalanma',
-    'case_instructions' => 'Resmi talimatlara göre aşağıdaki gibi hareket etmelisiniz:',
-    '1instruction1' => 'Karantinada kalmanıza gerek yok, ancak genel bir kural olarak,
-                        evde kalmalı. Evde kalamıyorsanız, lütfen en azından
-                         herkese 2 metrelik bir mesafe.',
+    'case1' => 'COVID-19\'dan kaynaklanan ciddi hastalıklar için düşük bir risk altındasınız ve belirtilerle ilgili ',
+    'result1' => ' Koronavirüsünüz olması muhtemel değildir.Hala riskinizi en aza indirmek için genel önlemlerin alınmasını öneririz.
+https://www.pio.gov.cy/coronavirus/ \'\' da ayrıntılı olarak verilen hastalığa yakalanma bilgileri bulunmaktadir.',
+    'case_instructions' => 'Resmi talimatlara göre şu şekilde hareket etmelisiniz:',
 
-    '1instruction2' => 'Sağlığınızı izleyin, özellikle 60 yaşın üzerindeyseniz veya
-                                        diyabet, akciğer hastalığı veya kalp hastalığı gibi kronik durum',
+    '1instruction1' => 'Karantinada kalmanıza gerek yok, ancak genel bir kural olarak, evde kalmalısiniz. Evde kalamıyorsanız, lütfen en azından herkese 2 metrelik bir mesafe koyun.',
+    '1instruction2' => 'Sağlığınızı izleyin, özellikle 60 yaşın üzerindeyseniz veya diyabet, akciğer hastalığı veya kalp hastalığı gibi kronik durumunuz varsa',
     '1instruction3' => 'Semptom geliştirmeniz durumunda, bu öz değerlendirme anketini tekrarlayın.',
-    '1instruction4' => 'COVID19 Acil Müdahale Hattı (1420), yalnızca
-                                        acil Durum.',
+    '1instruction4' => 'COVID19 Acil Müdahale Hattı (1420), yalnızca acil Durumlar icin.',
 
-    'case2' => 'COVID-19\'dan enfekte olma riski yükseksiniz, ancak
-                                        ilgili semptomlar',
-    'result2' => 'Belirtileriniz bir solunum virüsünün göstergesi değildir. Ancak, yaşınız nedeniyle
-                                    ve / veya kronik durumlarınız, eğer enfekte olursanız,
-                                    Daha
-                                    hastalığın şiddetli seyri.
-Hala riskinizi en aza indirmek için genel önlemlerin alınmasını öneririz.
-https://www.pio.gov.cy/coronavirus/ adresinde ayrıntılı olarak verilen hastalığa yakalanma',
+    'case2' => 'COVID-19\'dan enfekte olma riski yükseksiniz, ancak belirtilerle ilgili',
+    'result2' => 'Belirtileriniz bir solunum virüsünün göstergesi değildir. Ancak, yaşınız nedeniyle ve / veya kronik durumlarınız, eğer enfekte olursanız, hastalığın şiddetli daha yuksek.Hala riskinizi en aza indirmek için genel önlemlerin alınmasını öneririz. https://www.pio.gov.cy/coronavirus/ \'\' da ayrıntılı olarak verilen hastalığa yakalanma bilgileri bulunmaktadir.',
 
-    '2instruction1' => 'İki hafta tenha kaldı.',
-    '2instruction2' => 'Sağlığınızı izleyin',
-    '2instruction3' => 'In case you develop symptoms, contact your General Practitioner.
-                                        The COVID19 Emergency Response Line (1420) should be called only in case of
-                                        emergency.',
-    'case3' => 'COVID-19\'dan kaynaklanan ciddi hastalık riski düşüktür, ancak
-                                        
-                                        virüs',
-    'result3' => 'Seyahat geçmişiniz veya semptomlarınız COVID-19 ile enfekte olabileceğinizi gösterir.',
-    '3instruction1' => 'İleri muayenenizi ayarlayacak pratisyen hekiminize başvurun ve
-                                        tedavisi. *',
-    '3instruction2' => 'İki hafta karantinada kalın.',
-    '3instruction3' => 'Sağlığınızı izleyin, özellikle 60 yaşın üzerindeyseniz veya
-                                        diyabet, akciğer hastalığı veya kalp hastalığı gibi kronik durum.',
-    '3instruction4' => 'Semptom geliştirmeniz durumunda, bu öz değerlendirme anketini tekrarlayın.',
-
-    'case4' => 'Seyahat geçmişiniz ve semptomlarınız, bir solunum virüsünün göstergesidir.
-olmak
-                                        COVID-19.',
-
-    '4instruction1' => 'İleri muayenenizi ayarlayacak pratisyen hekiminize başvurun ve
-                                        tedavisi. *',
-    '4instruction2' => 'İki hafta karantinada kalın.',
-    '4instruction3' => 'COVID19 Acil Müdahale Hattı (1420), yalnızca
-                                        acil Durum.',
-    '4instruction4' => 'Yakın temasta bulunduklarınızı sağlıklarını izlemek, kalmak için bilgilendirin
-                                        mümkünse bu öz değerlendirme anketini doldurun.
-gerekirse talimatlar',
+    '2instruction1' => '2 hafta kapali kalin',
+    '2instruction2' => 'Sagliginizi inceleyin',
+    '2instruction3' => 'kendinizde hErhangi bir semptom gorurseniz, doktorunuzla iletisime geciniz. COVID19 Acil Müdahale Hattı (1420), yalnızca acil Durumlar icindir.',
+    'case3' => 'COVID-19\'dan kaynaklanan ciddi hastalık riski düşüktür, ancak virüs bulasabilir',
 
 
-    'case5' => 'COVID-19 sözleşmesi yapmış olabilirsiniz',
+    'result3' => ' Seyahat geçmişiniz veya belirtileriniz COVID-19 ile enfekte olabileceğinizi gösterir. ',
+    '3instruction1' => 'İleri muayenenizi ve tedavinizi ayarlayacak doktorunuza başvurun. *',
+    '3instruction2' => '2 hafta karantinada ',
+    '3instruction3' => 'Sağlığınızı izleyin, özellikle 60 yaşın üzerindeyseniz veya diyabet, akciğer hastalığı veya kalp hastalığı gibi kronik durumlariniz varsa. ',
+    '3instruction4' => 'Semptom geliştirmeniz durumunda, bu öz değerlendirme anketini tekrarlayın. ',
 
 
-    'result5' => 'Seyahat geçmişiniz ve / veya semptomlarınız, bir solunum virüsünün göstergesidir.
-olmak
-                                    COVID-19. Yaşınız ve / veya kronik durumunuz nedeniyle,
-                                    bir
-                                    hastalığın daha şiddetli seyri.',
+    'case4' => 'Seyahat geçmişiniz ve semptomlarınız, bir solunum virüsünün göstergesidir. COVID-19 olma olasiligi vardir. ',
 
-    '5instruction1' => 'COVID19 Acil Müdahale Hattı ile temasa geçin (1420)',
-    '5instruction2' => 'İki hafta karantinada kalın.',
-    '5instruction3' => 'Yakın temasta bulunduklarınızı sağlıklarını izlemek, kalmak için bilgilendirin
-                                        mümkünse bu öz değerlendirme anketini doldurun.
-gerekirse talimatlar',
-    'gender_other' => 'Belirtmemeyi tercih ederim',
-    'test_relying_on_guidelines' =>'Yönergeler Bu test aşağıdakilere dayanmaktadır',
+    '4instruction1' => 'İleri muayenenizi ve tedavisini ayarlayacak doktorunuza başvurun. *',
+    '4instruction2' => '2 hafta karantinada kalin.',
+    '4instruction3' => 'COVID19 Acil Müdahale Hattı (1420), yalnızca acil Durumlar icin.',
+    '4instruction4' => 'Yakın temasta bulunduklarınızı sağlıklarını izlemek, kalmak için bilgilendirin mümkünse bu öz değerlendirme anketini doldurun. Ayrica talimatlara bakin',
+
+
+    'case5' => 'COVID19 ile enfekte olabilirsiniz',
+
+
+    'result5' => ' Seyahat geçmişiniz ve / veya semptomlarınız, bir solunum virüsünün göstergesidir COVID-19 ile enfekte olmus olabilirsiniz. Yaşınız ve / veya kronik durumunuz nedeniyle,bir hastalığın daha şiddetli seyri. ',
+
+    '5instruction1' => 'COVID19 Acil Müdahale Hattı (1420) ile iletişime geçin ',
+    '5instruction2' => '2 hafta karantinada kalin',
+    '5instruction3' => 'Yakın temasta bulunduklarınızı sağlıklarını izlemek, kalmak için bilgilendirin mümkünse bu öz değerlendirme anketini doldurun. Ayrica talimatlara bakin',
+    'yes' => 'YES',
+    'no' => 'NO',
+    'test_relying_on_guidelines' => 'Yönergeler Bu test aşağıdakilere dayanmaktadır:',
+
+
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1196,6 +1196,18 @@
     });
 
 
+
+    $(document).ready(function() {
+        $(window).keydown(function(event){
+            if(event.keyCode == 13) {
+                event.preventDefault();
+                return false;
+            }
+        });
+    });
+
+
+
 </script>
 
 </html>
